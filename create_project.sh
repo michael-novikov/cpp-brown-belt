12d0b888bf3e8de4663f8d8223f711924290806d
michael-novikov <michael.a.novikov@yandex.com>
2020-04-30 11:21:39 +0300

    add utilities

---
+++ create_project.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+project_name=$1
+main_file_link=$2
+root_dir=$PWD
+project_dir=$root_dir/$project_name
+build_dir=$root_dir/build/$project_name
+main_file_name=$project_dir/$project_name\.cpp
+project_file_template=$root_dir/CMakeLists.txt.template
+project_file=$project_dir/CMakeLists.txt
+generate_build_files_command='cmake $project_dir'
+compiler_commands_file='compile_commands.json'
+
+mkdir $project_dir
+mkdir -p $build_dir
+
+cp $project_file_template $project_dir
+
+eval "sed -i.bak 's/THIS_PROJECT_NAME/$project_name/g' $project_file"
+rm ${project_file}.bak
+
+wget -O $main_file_name "$main_file_link"
+
+cd $build_dir
+eval $generate_build_files_command
+eval $generate_build_files_command
+
+cd $project_dir
+ln -s $build_dir/$compiler_commands_file $compiler_commands_file
+
+echo 'project configured, have a nice coding session, bro'
+
---
+++ utility/profile.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <string>
+
+class LogDuration {
+public:
+  explicit LogDuration(const std::string& msg = "")
+    : message(msg + ": ")
+    , start(std::chrono::steady_clock::now())
+  {
+  }
+
+  ~LogDuration() {
+    auto finish = std::chrono::steady_clock::now();
+    auto dur = finish - start;
+    std::cerr << message
+      << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count()
+      << " ms" << endl;
+  }
+private:
+  std::string message;
+  std::chrono::steady_clock::time_point start;
+};
+
+#define UNIQ_ID_IMPL(lineno) _a_local_var_##lineno
+#define UNIQ_ID(lineno) UNIQ_ID_IMPL(lineno)
+
+#define LOG_DURATION(message) \
+  LogDuration UNIQ_ID(__LINE__){message};
+
