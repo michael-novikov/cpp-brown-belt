1232782390560f4be69fbf6d22c8aa25aa63dc3b
michael-novikov <michael.a.novikov@yandex.com>
2020-05-04 15:10:37 +0300

    zoo - store ptrs to animals to implement polymorphism

--- zoo/zoo.cpp
+++ zoo/zoo.cpp
@@ -6,27 +6,22 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <memory>
 
 using namespace std;
 
-using Zoo = vector<Animal>;
+using Zoo = vector<unique_ptr<Animal>>;
 
-// Эта функция получает на вход поток ввода и читает из него описание зверей.
-// Если очередное слово этого текста - Tiger, Wolf или Fox, функция должна поместить соответствующего зверя в зоопарк.
-// В противном случае она должна прекратить чтение и сгенерировать исключение runtime_error.
 Zoo CreateZoo(istream& in) {
   Zoo zoo;
   string word;
   while (in >> word) {
     if (word == "Tiger") {
-      Tiger t;
-      zoo.push_back(t);
+      zoo.push_back(make_unique<Tiger>());
     } else if (word == "Wolf") {
-      Wolf w;
-      zoo.push_back(w);
+      zoo.push_back(make_unique<Wolf>());
     } else if (word == "Fox") {
-      Fox f;
-      zoo.push_back(f);
+      zoo.push_back(make_unique<Fox>());
     } else {
       throw runtime_error("Unknown animal!");
     }
@@ -34,12 +29,9 @@ Zoo CreateZoo(istream& in) {
   return zoo;
 }
 
-// Эта функция должна перебрать всех зверей в зоопарке в порядке их создания
-// и записать в выходной поток для каждого из них результат работы виртуальной функции voice.
-// Разделяйте голоса разных зверей символом перевода строки '\n'.
 void Process(const Zoo& zoo, ostream& out) {
   for (const auto& animal : zoo) {
-    out << animal.Voice() << "\n";
+    out << animal->Voice() << "\n";
   }
 }
 
