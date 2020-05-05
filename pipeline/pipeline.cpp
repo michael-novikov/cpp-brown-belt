6935e3f30828925ea585538cb699097d497117c5
michael-novikov <michael.a.novikov@yandex.com>
2020-05-05 11:19:36 +0300

    pipeline - add workers and pipeline builder

--- pipeline/pipeline.cpp
+++ pipeline/pipeline.cpp
@@ -21,64 +21,152 @@ public:
   virtual ~Worker() = default;
   virtual void Process(unique_ptr<Email> email) = 0;
   virtual void Run() {
-    // только первому worker-у в пайплайне нужно это имплементировать
+    // should be implemented for the first worker in pipeline
     throw logic_error("Unimplemented");
   }
 
 protected:
-  // реализации должны вызывать PassOn, чтобы передать объект дальше
-  // по цепочке обработчиков
   void PassOn(unique_ptr<Email> email) const;
 
 public:
   void SetNext(unique_ptr<Worker> next);
+
+private:
+  unique_ptr<Worker> next_;
 };
 
+void Worker::PassOn(unique_ptr<Email> email) const {
+  if (next_) {
+    next_->Process(move(email));
+  }
+}
+
+void Worker::SetNext(unique_ptr<Worker> next) {
+  next_ = move(next);
+}
 
 class Reader : public Worker {
 public:
-  // реализуйте класс
+  explicit Reader(istream& in);
+  virtual void Process(unique_ptr<Email> email) override;
+  void Run() override;
+private:
+  istream& in_;
 };
 
+unique_ptr<Email> ReadEmail(istream& in) {
+  string from, to, body;
+  bool read = getline(in, from)
+    && getline(in, to)
+    && getline(in, body);
+  return read ? make_unique<Email>(Email{from, to, body}) : nullptr;
+}
+
+Reader::Reader(istream& in) : in_(in) {}
+
+void Reader::Process(unique_ptr<Email> email) {
+  PassOn(move(email));
+}
+
+void Reader::Run() {
+  bool keep_read{true};
+  while (keep_read) {
+    auto email = ReadEmail(in_);
+    if (email) {
+      Process(move(email));
+    }
+    else {
+      keep_read = false;
+    }
+  }
+}
 
 class Filter : public Worker {
 public:
   using Function = function<bool(const Email&)>;
 
-public:
-  // реализуйте класс
+  explicit Filter(Function f) : filter_func(f) {}
+  virtual void Process(unique_ptr<Email> email) override;
+private:
+  Function filter_func;
 };
 
+void Filter::Process(unique_ptr<Email> email) {
+  if (filter_func(*email)) {
+    PassOn(move(email));
+  }
+}
 
 class Copier : public Worker {
 public:
-  // реализуйте класс
+  explicit Copier(string to) : to_(move(to)) {}
+  virtual void Process(unique_ptr<Email> email) override;
+private:
+  string to_;
 };
 
+void Copier::Process(unique_ptr<Email> email) {
+  unique_ptr<Email> copy;
+  if (to_ != email->to) {
+    copy = make_unique<Email>(Email{email->from, to_, email->body});
+  }
+
+  PassOn(move(email));
+
+  if (copy) {
+    PassOn(move(copy));
+  }
+}
 
 class Sender : public Worker {
 public:
-  // реализуйте класс
+  explicit Sender(ostream& out) : out_(out) {}
+  virtual void Process(unique_ptr<Email> email) override;
+private:
+  ostream& out_;
 };
 
+void Sender::Process(unique_ptr<Email> email) {
+  out_
+    << email->from << endl
+    << email->to << endl
+    << email->body << endl;
 
-// реализуйте класс
+  PassOn(move(email));
+}
+
 class PipelineBuilder {
 public:
-  // добавляет в качестве первого обработчика Reader
-  explicit PipelineBuilder(istream& in);
+  explicit PipelineBuilder(istream& in)
+  {
+    pipeline.push_back(make_unique<Reader>(in));
+  }
 
-  // добавляет новый обработчик Filter
-  PipelineBuilder& FilterBy(Filter::Function filter);
+  PipelineBuilder& FilterBy(Filter::Function filter) {
+    pipeline.push_back(make_unique<Filter>(filter));
+    return *this;
+  }
 
-  // добавляет новый обработчик Copier
-  PipelineBuilder& CopyTo(string recipient);
+  PipelineBuilder& CopyTo(string recipient) {
+    pipeline.push_back(make_unique<Copier>(recipient));
+    return *this;
+  }
 
-  // добавляет новый обработчик Sender
-  PipelineBuilder& Send(ostream& out);
+  PipelineBuilder& Send(ostream& out) {
+    pipeline.push_back(make_unique<Sender>(out));
+    return *this;
+  }
 
-  // возвращает готовую цепочку обработчиков
-  unique_ptr<Worker> Build();
+  unique_ptr<Worker> Build() {
+    if (pipeline.size() > 1) {
+      for (int cur = pipeline.size() - 2; cur >= 0; --cur) {
+        pipeline[cur]->SetNext(move(pipeline[cur + 1]));
+      }
+    }
+    return move(pipeline.front());
+  }
+private:
+  vector<unique_ptr<Worker>> pipeline;
 };
 
 
