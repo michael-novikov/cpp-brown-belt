ed588daec74dc53be8c3c62379ee9d28209a5c3b
michael-novikov <michael.a.novikov@yandex.com>
2020-05-06 16:48:17 +0300

    expression_tree - implement expression nodes

--- expression_tree/Common.h
+++ expression_tree/Common.h
@@ -3,22 +3,19 @@
 #include <memory>
 #include <string>
 
-// Базовый класс арифметического выражения
 class Expression {
 public:
   virtual ~Expression() = default;
 
-  // Вычисляет значение выражения
   virtual int Evaluate() const = 0;
 
-  // Форматирует выражение как строку
-  // Каждый узел берётся в скобки, независимо от приоритета
+  // Wraps each node in brackets regardless of prioriry
   virtual std::string ToString() const = 0;
 };
 
 using ExpressionPtr = std::unique_ptr<Expression>;
 
-// Функции для формирования выражения
+// Creation functions
 ExpressionPtr Value(int value);
 ExpressionPtr Sum(ExpressionPtr left, ExpressionPtr right);
 ExpressionPtr Product(ExpressionPtr left, ExpressionPtr right);
