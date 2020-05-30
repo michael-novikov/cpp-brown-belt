dacce3b65f95b4ee7ae8c38f09580351e7d70258
michael-novikov <michael.a.novikov@yandex.com>
2020-05-30 15:40:02 +0300

    demographic_stats_tests - add unit tests

--- demographic_stats_tests/demographic_stats_tests.cpp
+++ demographic_stats_tests/demographic_stats_tests.cpp
@@ -133,8 +133,259 @@ void PrintStats(const AgeStats& stats,
              << "Median age for unemployed males = "
              << stats.unemployed_males   << endl;
 }
+
+void TestPersonEqual() {
+  ASSERT_EQUAL((Person{18, Gender::FEMALE, true}), (Person{18, Gender::FEMALE, true}));
+
+  ASSERT((Person{18, Gender::FEMALE, true} == Person{18, Gender::FEMALE, true}));
+  ASSERT((Person{20, Gender::FEMALE, true} == Person{20, Gender::FEMALE, true}));
+  ASSERT((Person{18, Gender::MALE, true} == Person{18, Gender::MALE, true}));
+  ASSERT((Person{18, Gender::FEMALE, false} == Person{18, Gender::FEMALE, false}));
+
+  ASSERT(!(Person{18, Gender::FEMALE, true} == Person{19, Gender::FEMALE, true}));
+  ASSERT(!(Person{18, Gender::FEMALE, true} == Person{18, Gender::MALE, true}));
+  ASSERT(!(Person{18, Gender::FEMALE, true} == Person{18, Gender::FEMALE, false}));
+}
+
+void TestPersonPrint() {
+  {
+    ostringstream actual;
+    actual << Person{18, Gender::FEMALE, true};
+    string expected = "Person(age=18, gender=0, is_employed=true)";
+    ASSERT_EQUAL(actual.str(), expected);
+  }
+  {
+    ostringstream actual;
+    actual << Person{19, Gender::FEMALE, true};
+    string expected = "Person(age=19, gender=0, is_employed=true)";
+    ASSERT_EQUAL(actual.str(), expected);
+  }
+  {
+    ostringstream actual;
+    actual << Person{18, Gender::MALE, true};
+    string expected = "Person(age=18, gender=1, is_employed=true)";
+    ASSERT_EQUAL(actual.str(), expected);
+  }
+  {
+    ostringstream actual;
+    actual << Person{18, Gender::FEMALE, false};
+    string expected = "Person(age=18, gender=0, is_employed=false)";
+    ASSERT_EQUAL(actual.str(), expected);
+  }
+}
+
+void TestComputeMedianAge() {
+  { // Test empty
+    vector<Person> empty;
+    ASSERT_EQUAL(ComputeMedianAge(begin(empty), end(empty)), 0);
+  }
+  { // Test 1 element
+    vector<Person> one_element = { Person{18, Gender::FEMALE, true} };
+    ASSERT_EQUAL(ComputeMedianAge(begin(one_element), end(one_element)), 18);
+  }
+  { // Test even number of element
+    vector<Person> even_number_of_elements = { Person{18, Gender::FEMALE, true}, Person{20, Gender::FEMALE, true} };
+    ASSERT_EQUAL(ComputeMedianAge(begin(even_number_of_elements), end(even_number_of_elements)), 20);
+  }
+  { // Test even number of element not sorted
+    vector<Person> even_not_sorted = { Person{20, Gender::FEMALE, true}, Person{18, Gender::FEMALE, true} };
+    ASSERT_EQUAL(ComputeMedianAge(begin(even_not_sorted), end(even_not_sorted)), 20);
+  }
+  { // Test odd number of element
+    vector<Person> odd_number_of_elements = { Person{18, Gender::FEMALE, true}, Person{20, Gender::FEMALE, true}, Person{22, Gender::FEMALE, true} };
+    ASSERT_EQUAL(ComputeMedianAge(begin(odd_number_of_elements), end(odd_number_of_elements)), 20);
+  }
+  { // Test odd number of element
+    vector<Person> odd_not_sorted = { Person{20, Gender::FEMALE, true}, Person{18, Gender::FEMALE, true}, Person{22, Gender::FEMALE, true} };
+    ASSERT_EQUAL(ComputeMedianAge(begin(odd_not_sorted), end(odd_not_sorted)), 20);
+  }
+}
+
+void TestReadPersons() {
+  { // Test empty
+    istringstream empty {"0"};
+    ASSERT_EQUAL(ReadPersons(empty), vector<Person>{});
+  }
+  { // Test 1 element
+    istringstream one_element {"1 18 0 1"};
+    ASSERT_EQUAL(ReadPersons(one_element), (vector<Person>{ {18, Gender::FEMALE, true} }));
+  }
+  { // Test 2 element
+    istringstream two_element {"2 18 0 1 20 1 0"};
+    ASSERT_EQUAL(ReadPersons(two_element), (vector<Person>{ {18, Gender::FEMALE, true}, {20, Gender::MALE, false} }));
+  }
+}
+
+void TestComputeStats() {
+  auto age_stats_equal = [](const AgeStats& lhs, const AgeStats& rhs) -> bool {
+    return lhs.total == rhs.total
+      && lhs.females == rhs.females
+      && lhs.males == rhs.males
+      && lhs.employed_females == rhs.employed_females
+      && lhs.unemployed_females == rhs.unemployed_females
+      && lhs.employed_males == rhs.employed_males
+      && lhs.unemployed_males == rhs.unemployed_males;
+  };
+
+  { // Test empty
+    ASSERT(age_stats_equal(
+        ComputeStats(vector<Person>{}),
+        AgeStats{ 0, 0, 0, 0, 0, 0, 0 }
+    ));
+  }
+  { // Test one employed female
+    ASSERT(age_stats_equal(
+        ComputeStats(vector<Person>{ {18, Gender::FEMALE, true} }),
+        AgeStats{ 18, 18, 0, 18, 0, 0, 0 }
+    ));
+  }
+  { // Test one unemployed female
+    ASSERT(age_stats_equal(
+        ComputeStats(vector<Person>{ {18, Gender::FEMALE, false} }),
+        AgeStats{ 18, 18, 0, 0, 18, 0, 0 }
+    ));
+  }
+  { // Test one employed male
+    ASSERT(age_stats_equal(
+        ComputeStats(vector<Person>{ {20, Gender::MALE, true} }),
+        AgeStats{ 20, 0, 20, 0, 0, 20, 0 }
+    ));
+  }
+  { // Test one unemployed male
+    ASSERT(age_stats_equal(
+        ComputeStats(vector<Person>{ {20, Gender::MALE, false} }),
+        AgeStats{ 20, 0, 20, 0, 0, 0, 20 }
+    ));
+  }
+  { // Test 1 + 1 females
+    ASSERT(age_stats_equal(
+        ComputeStats(vector<Person>{ {20, Gender::FEMALE, true}, {22, Gender::FEMALE, false} }),
+        AgeStats{ 22, 22, 0, 20, 22, 0, 0 }
+    ));
+  }
+  { // Test 1 + 1 males
+    ASSERT(age_stats_equal(
+        ComputeStats(vector<Person>{ {20, Gender::MALE, true}, {22, Gender::MALE, false} }),
+        AgeStats{ 22, 0, 22, 0, 0, 20, 22 }
+    ));
+  }
+  { // Test 2 + 2 males and females
+    ASSERT(age_stats_equal(
+        ComputeStats(vector<Person>{ {19, Gender::FEMALE, true}, {18, Gender::FEMALE, false}, {21, Gender::MALE, true}, {20, Gender::MALE, false} }),
+        AgeStats{ 20, 19, 21, 19, 18, 21, 20 }
+    ));
+  }
+}
+
+void TestPrintStats() {
+  { // Test empty
+    string expected = R"(Median age = 0
+Median age for females = 0
+Median age for males = 0
+Median age for employed females = 0
+Median age for unemployed females = 0
+Median age for employed males = 0
+Median age for unemployed males = 0
+)";
+
+    AgeStats empty { 0, 0, 0, 0, 0, 0, 0 };
+    ostringstream stats_str;
+    PrintStats(empty, stats_str);
+
+    ASSERT_EQUAL(stats_str.str(), expected);
+  }
+  { // Test prints all values
+    string expected = R"(Median age = 1
+Median age for females = 2
+Median age for males = 3
+Median age for employed females = 4
+Median age for unemployed females = 5
+Median age for employed males = 6
+Median age for unemployed males = 7
+)";
+
+    AgeStats empty { 1, 2, 3, 4, 5, 6, 7 };
+    ostringstream stats_str;
+    PrintStats(empty, stats_str);
+
+    ASSERT_EQUAL(stats_str.str(), expected);
+  }
+  { // Test 1 employed females
+    string expected = R"(Median age = 18
+Median age for females = 18
+Median age for males = 0
+Median age for employed females = 18
+Median age for unemployed females = 0
+Median age for employed males = 0
+Median age for unemployed males = 0
+)";
+
+    AgeStats employed_females { 18, 18, 0, 18, 0, 0, 0 };
+    ostringstream stats_str;
+    PrintStats(employed_females, stats_str);
+
+    ASSERT_EQUAL(stats_str.str(), expected);
+  }
+  { // Test 1 unemployed female
+    string expected = R"(Median age = 18
+Median age for females = 18
+Median age for males = 0
+Median age for employed females = 0
+Median age for unemployed females = 18
+Median age for employed males = 0
+Median age for unemployed males = 0
+)";
+
+    AgeStats unemployed_females { 18, 18, 0, 0, 18, 0, 0 };
+    ostringstream stats_str;
+    PrintStats(unemployed_females, stats_str);
+
+    ASSERT_EQUAL(stats_str.str(), expected);
+  }
+  { // Test 1 employed male
+    string expected = R"(Median age = 18
+Median age for females = 0
+Median age for males = 18
+Median age for employed females = 0
+Median age for unemployed females = 0
+Median age for employed males = 18
+Median age for unemployed males = 0
+)";
+
+    AgeStats employed_females { 18, 0, 18, 0, 0, 18, 0 };
+    ostringstream stats_str;
+    PrintStats(employed_females, stats_str);
+
+    ASSERT_EQUAL(stats_str.str(), expected);
+  }
+  { // Test 1 unemployed male
+    string expected = R"(Median age = 18
+Median age for females = 0
+Median age for males = 18
+Median age for employed females = 0
+Median age for unemployed females = 0
+Median age for employed males = 0
+Median age for unemployed males = 18
+)";
+
+    AgeStats employed_females { 18, 0, 18, 0, 0, 0, 18 };
+    ostringstream stats_str;
+    PrintStats(employed_females, stats_str);
+
+    ASSERT_EQUAL(stats_str.str(), expected);
+  }
+}
+
 int main() {
-  PrintStats(ComputeStats(ReadPersons()));
+  TestRunner tr;
+
+  RUN_TEST(tr, TestPersonEqual);
+  RUN_TEST(tr, TestPersonPrint);
+  RUN_TEST(tr, TestComputeMedianAge);
+  RUN_TEST(tr, TestReadPersons);
+  RUN_TEST(tr, TestComputeStats);
+  RUN_TEST(tr, TestPrintStats);
+
   return 0;
 }
 
